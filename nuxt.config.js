6ee09a7e2c39233766afeeab93fe9c4c0c029376
Jimmy <791092326@qq.com>
2022-02-14 10:24:10 +0800

    axios跨域

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,12 @@ module.exports = {
     '@nuxtjs/axios'
   ],
   axios: {
-    baseURL: '', // Used as fallback if no runtime config is provided
+    //baseURL: 'http://192.168.5.28:3000', // Used as fallback if no runtime config is provided
+    proxy: process.env.NODE_ENV !== 'production' ? true : false
+  },
+
+  proxy: {
+    '/api/': { target: 'http://1.116.147.186:3000'}
   },
 
   /*
